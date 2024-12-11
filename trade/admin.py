796bed49470270d533aa759e6febd842f0bfbaf0
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2024-12-11 23:40:05 +0300

    models.py, admin.py, settings.py were modified.

--- trade/admin.py
+++ trade/admin.py
@@ -1,13 +1,31 @@
 from django.contrib import admin
 
-from trade.models import TradeCompany
+from trade.models import TradeCompany, Contact, Product
 
 # Register your models here.
 admin.site.header = 'Админка'
 
+@admin.register(Contact)
+class AdminContact(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'city', )
+    list_filter = ('country', 'city', 'street')
 
-admin.site.register(TradeCompany)
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('id', 'name', 'product_model', 'release_date', 'trade_company__name')
+    list_filter = ('name', 'brand', 'trade_company__name')
+    search_fields = ('id', 'name', 'product_model')
+
+
+@admin.register(TradeCompany)
 class AdminTradeCompany(admin.ModelAdmin):
     list_display = ('supplier__name', )
     list_filter = ('contact__city', )
-    
+    ordering = ('name', )
+    actions = ('set_null_to_ending_balance', )
+
+    @admin.action(description='Обнулить задолженность')
+    def set_null_to_ending_balance(self, request, queryset):
+        queryset.update(supplier_ending_balance=0)
+        self.message_user(request, "Задолженности перед поставщиками успешно обнулены.")
--- trade/models.py
+++ trade/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
 
 # Create your models here.
 NULLABLE = dict(null=True, blank=True)
+
 
 class Contact(models.Model):
     email = models.EmailField(max_length=150, verbose_name='электронная почта')
@@ -13,23 +16,50 @@ class Contact(models.Model):
     def __str__(self):
         return f'{self.email}, {self.country}, {self.city}, {self.street}, {self.house}'
 
+    class Meta:
+        verbose_name = 'контакт'
+        verbose_name_plural = 'контакты'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=250, help_text='введите наименование продукта',
                             verbose_name='наименование продукта')
-    product_model = models.CharField(max_length=250, help_text='введите модель', unique=True, verbose_name='название модели')
-    release_date = models.DateField(help_text='дата выпуска продукта в продажу', verbose_name='Дата выпуска продукта')
+    brand = models.CharField(max_length=150, verbose_name='торговая марка', **NULLABLE)
+    product_model = models.CharField(max_length=250, help_text='введите модель', unique=True,
+                                     verbose_name='название модели',  **NULLABLE)
+    release_date = models.DateField(help_text='дата выпуска продукта в продажу', verbose_name='Дата выпуска продукта',
+                                    **NULLABLE)
+    trade_company = models.ForeignKey('TradeCompany', on_delete=models.SET_NULL, verbose_name='компания', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} - {self.product_model}'
 
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
 class TradeCompany(models.Model):
     name = models.CharField(max_length=250, help_text='Название компании')
     contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, verbose_name='ссылка на контакты', **NULLABLE)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, verbose_name='ссылка на контакты', **NULLABLE)
     supplier = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='поставщик', **NULLABLE)
     supplier_ending_balance = models.FloatField(default=0, verbose_name='задолженность перед поставщиком')
     creation_date = models.TimeField(auto_now_add=True, verbose_name='дата создания')
 
+    def clean(self):
+        print(str(self.supplier_ending_balance))
+        if self.supplier_ending_balance < 0:
+            raise ValidationError('Задолженность не может быть отрицательной.')
+        if self.supplier == self:
+            raise ValidationError('Компания не может быть поставщиком для себя.')
+        if len(re.split(r'[ ,.]', str(self.supplier_ending_balance))[1]) > 2:
+            raise ValidationError('Допустимое число знаков после запятой в задолженности - 2.')
+        return self
+
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'компания'
+        verbose_name_plural = 'компании'
+        ordering = ('name', )
